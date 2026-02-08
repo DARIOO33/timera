5c45d5b798d61bc1336fccd0ce2b182f048580d9
DARIOO33 <darioanwar3@gmail.com>
2026-02-08 21:32:12 +0100

    update meta pixel ID

--- app/layout.js
+++ app/layout.js
@@ -32,7 +32,7 @@ export default function RootLayout({ children }) {
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '796632352707426');
+            fbq('init', '1677105643270199');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -43,7 +43,7 @@ export default function RootLayout({ children }) {
             height="1"
             width="1"
             style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=796632352707426&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1677105643270199&ev=PageView&noscript=1"
           />
         </noscript>
 
