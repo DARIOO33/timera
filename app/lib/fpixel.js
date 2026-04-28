626fa1cd54c8226507aee266223112eb19edd632
Anwer Dario Aissaoui <103328505+DARIOO33@users.noreply.github.com>
2026-04-28 10:22:06 +0100

    Update fpixel.js

--- app/lib/fpixel.js
+++ app/lib/fpixel.js
@@ -1,11 +1,9 @@
-"use client"
+"use client";
 
 export const fbPixel = {
-    track: (event, data = {}) => {
-        if (typeof window !== "undefined" && window.fbq) {
-            window.fbq("track", event, data);
-        } else {
-            console.warn("FB Pixel not ready");
-        }
-    },
+  track: (event, data = {}) => {
+    if (typeof window !== "undefined" && window.fbq) {
+      window.fbq("track", event, data);
+    }
+  },
 };
